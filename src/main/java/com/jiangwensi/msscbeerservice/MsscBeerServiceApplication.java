e1fd54c27a6dfa5e36f63b6e68f524b9513a8285
JIANG WENSI <jiangwensi@hotmail.com>
2020-11-08 13:18:05 +0800

    Configure JMS for services

--- src/main/java/com/jiangwensi/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/com/jiangwensi/msscbeerservice/MsscBeerServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication//(exclude = ArtemisAutoConfiguration.class)
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/jiangwensi/msscbeerservice/config/JMSConfig.java
@@ -0,0 +1,21 @@
+package com.jiangwensi.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by Jiang Wensi on 8/11/2020
+ */
+@Configuration
+public class JMSConfig {
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
