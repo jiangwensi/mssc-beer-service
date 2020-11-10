06e1bb41156ff5d0e7bab7f0414c9e2bc3659b8f
JIANG WENSI <jiangwensi@hotmail.com>
2020-11-10 11:04:46 +0800

    handle validate order response

--- src/main/java/guru/springframework/msscbeerservice/services/order/BeerOrderValidationListener.java
+++ src/main/java/guru/springframework/msscbeerservice/services/order/BeerOrderValidationListener.java
@@ -1,11 +1,8 @@
 package guru.springframework.msscbeerservice.services.order;
 
-import guru.sfg.brewery.model.BeerOrderDto;
-import guru.sfg.brewery.model.BeerOrderLineDto;
 import guru.sfg.brewery.model.ValidateOrderRequest;
 import guru.sfg.brewery.model.ValidateOrderResult;
 import guru.springframework.msscbeerservice.config.JMSConfig;
-import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
@@ -26,7 +23,7 @@ public class BeerOrderValidationListener {
         Boolean isValid = validator.validateOrder(validateOrderRequest.getBeerOrderDto());
 
         jmsTemplate.convertAndSend(
-                JMSConfig.VALIDATE_ORDER_RESULT_QUEUE,
+                JMSConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
                 ValidateOrderResult.builder()
                         .isValid(isValid)
                         .orderId(validateOrderRequest.getBeerOrderDto().getId())
