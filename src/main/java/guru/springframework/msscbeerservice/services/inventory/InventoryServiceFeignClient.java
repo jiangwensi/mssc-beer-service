c9ee54d99853f0b1b4e4474a2bfcae721f27ab27
JIANG WENSI <jiangwensi@hotmail.com>
2020-11-12 14:56:05 +0800

    update inventory service to use openfeign

---
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbeerservice.services.inventory;
+
+import guru.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Jiang Wensi on 12/11/2020
+ */
+@FeignClient(name="inventory-service")
+public interface InventoryServiceFeignClient {
+    @RequestMapping(method= RequestMethod.GET, value=BeerInventoryServiceRestTemplate.INVENTORY_PATH)
+    ResponseEntity<List<BeerInventoryDto>> getOnhandInventory(@PathVariable UUID beerId);
+}
